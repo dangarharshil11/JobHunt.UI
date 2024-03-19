af6d2e91b424d4051530274ba858383514dc9463
hdangar <hdangar@synoptek.com>
2024-03-19 10:22:38 +0530

    Added Form Validation to all Edit Pages of Employer Routes

--- src/app/features/employer/edit-company-details/edit-company-details.component.ts
+++ src/app/features/employer/edit-company-details/edit-company-details.component.ts
@@ -1,10 +1,11 @@
-import { Component, OnDestroy, OnInit } from '@angular/core';
+import { ChangeDetectorRef, Component, OnDestroy, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { Subscription } from 'rxjs';
 
 import { Organization } from '../models/organization.model';
 import { EmployerService } from '../services/employer.service';
 import { MessageService } from 'primeng/api';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-edit-company-details',
@@ -14,12 +15,11 @@ import { MessageService } from 'primeng/api';
 export class EditCompanyDetailsComponent implements OnInit, OnDestroy {
   profile: Organization;
   email?: string | null = null;
-  error: string = '';
-
+  
   editProfileSubscription?: Subscription;
 
-  constructor(private route: ActivatedRoute, private router: Router, private employerService: EmployerService, private messageService: MessageService){
-    this.profile ={
+  constructor(private route: ActivatedRoute, private router: Router, private employerService: EmployerService, private messageService: MessageService, private fb: FormBuilder, private changeDetector: ChangeDetectorRef) {
+    this.profile = {
       organization: '',
       organizationType: '',
       companyEmail: '',
@@ -31,12 +31,33 @@ export class EditCompanyDetailsComponent implements OnInit, OnDestroy {
     }
   }
 
+  editCompanyForm = this.fb.group({
+    organization: ['', Validators.required],
+    organizationType: ['', Validators.required],
+    companyEmail: ['', Validators.required],
+    companyPhone: ['', Validators.minLength(10)],
+    noOfEmployees: [1, Validators.min(1)],
+    startYear: new FormControl(2024, [Validators.min(1800), Validators.max(2024), Validators.required]),
+    about: ['', Validators.minLength(50)],
+  });
+
   ngOnInit(): void {
     this.email = localStorage.getItem('user-email');
-    if(this.email){
+    if (this.email) {
       this.employerService.getprofile(this.email).subscribe({
         next: (response) => {
-          this.profile = response.result;
+          if (response.isSuccess) {
+            this.profile = response.result;
+            this.editCompanyForm.setValue({
+              organization: this.profile.organization,
+              organizationType: this.profile.organizationType,
+              companyPhone: this.profile.companyPhone,
+              companyEmail: this.profile.companyEmail,
+              noOfEmployees: this.profile.noOfEmployees,
+              startYear: this.profile.startYear,
+              about: this.profile.about,
+            })
+          }
         },
         error: (error) => {
           console.error(error);
@@ -45,28 +66,37 @@ export class EditCompanyDetailsComponent implements OnInit, OnDestroy {
     }
   }
 
-  onFormSubmit(){
-    if(this.profile.organization.trim() == '' || this.profile.organizationType.trim() == '' || this.profile.about.trim() == '' || 
-      this.profile.startYear == 0 || this.profile.companyEmail.trim() == '' || this.profile.companyPhone.trim() == '' || 
-      this.profile.noOfEmployees == 0){
-        this.error = 'Please Enter all the Details';
+  ngAfterContentChecked(): void {
+    this.changeDetector.detectChanges();
+  }
+
+  onFormSubmit() {
+    console.log(this.editCompanyForm.value);
+    this.profile = {
+      organization: this.editCompanyForm.get('organization')?.value || this.profile.organization,
+      organizationType: this.editCompanyForm.get('organizationType')?.value || this.profile.organizationType,
+      companyEmail: this.editCompanyForm.get('companyEmail')?.value || this.profile.companyEmail,
+      companyPhone: this.editCompanyForm.get('companyPhone')?.value || this.profile.companyPhone,
+      noOfEmployees: this.editCompanyForm.get('noOfEmployees')?.value || this.profile.noOfEmployees,
+      startYear: this.editCompanyForm.get('startYear')?.value || this.profile.startYear,
+      about: this.editCompanyForm.get('about')?.value || this.profile.about,
+      createdBy: this.profile.createdBy,
     }
-    else{
-      this.editProfileSubscription = this.employerService.updateProfile(this.profile).subscribe({
-        next: (response) => {
-          if(response.isSuccess){
-            this.show();
-            this.router.navigateByUrl(`/profile/${response.result.createdBy}`)
-          }
-          else{
-            this.error = response.message;
-          }
-        },
-        error: (error) => {
-          console.error(error);
+
+    this.editProfileSubscription = this.employerService.updateProfile(this.profile).subscribe({
+      next: (response) => {
+        if (response.isSuccess) {
+          this.show();
+          this.router.navigateByUrl(`/profile/${response.result.createdBy}`)
         }
-      });
-    }
+        else {
+          this.error(response.message);
+        }
+      },
+      error: (error) => {
+        console.error(error);
+      }
+    });
   }
 
   ngOnDestroy(): void {
@@ -75,5 +105,9 @@ export class EditCompanyDetailsComponent implements OnInit, OnDestroy {
 
   show() {
     this.messageService.add({ severity: 'success', summary: 'Success', detail: 'Organization Information Updated Successfully!' });
+  }
+
+  error(msg: string) {
+    this.messageService.add({ severity: 'error', summary: 'Error', detail: msg });
   }
 }
--- src/app/features/employer/edit-vacancy/edit-vacancy.component.ts
+++ src/app/features/employer/edit-vacancy/edit-vacancy.component.ts
@@ -5,6 +5,7 @@ import { Subscription } from 'rxjs';
 import { EmployerService } from '../services/employer.service';
 import { VacancyRequest } from '../models/vacancy-request.model';
 import { MessageService } from 'primeng/api';
+import { FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-edit-vacancy',
@@ -13,13 +14,12 @@ import { MessageService } from 'primeng/api';
 })
 export class EditVacancyComponent implements OnInit, OnDestroy {
   model: VacancyRequest;
-  error: string = '';
   email: string | null = null;
   id: string | null = null;
   
   editVacancySubscription?: Subscription;
 
-  constructor(private employerService: EmployerService, private router: Router, private route: ActivatedRoute, private messageService: MessageService){
+   constructor(private employerService: EmployerService, private router: Router, private route: ActivatedRoute, private messageService: MessageService, private fb: FormBuilder){
     this.model = {
       publishedBy: '',
       publishedDate: new Date(),
@@ -34,6 +34,16 @@ export class EditVacancyComponent implements OnInit, OnDestroy {
     }
   }
 
+  editVacancyForm = this.fb.group({
+    noOfVacancies: [0, Validators.min(1)],
+    minimumQualification: ['', Validators.required],
+    jobTitle: ['', Validators.required],
+    jobDescription: ['', Validators.minLength(50)],
+    experienceRequired: ['', Validators.required],
+    minimumSalary: [0, Validators.min(1)],
+    maximumSalary: [0, Validators.min(1)],
+  });
+
   ngOnInit(): void {
     this.route.paramMap.subscribe({
       next: (response) => {
@@ -46,6 +56,15 @@ export class EditVacancyComponent implements OnInit, OnDestroy {
         next: (response) => {
           if(response.isSuccess){
             this.model = response.result;
+            this.editVacancyForm.setValue({
+              noOfVacancies: this.model.noOfVacancies,
+              minimumQualification: this.model.minimumQualification,
+              jobTitle: this.model.jobTitle,
+              jobDescription: this.model.jobDescription,
+              experienceRequired: this.model.experienceRequired,
+              minimumSalary: this.model.minimumSalary,
+              maximumSalary: this.model.maximumSalary
+            })
           }
         },
         error: (error) => {
@@ -53,17 +72,33 @@ export class EditVacancyComponent implements OnInit, OnDestroy {
         }
       })
     }
-    
   }
 
   onFormSubmit(){
+    this.model = {
+      jobTitle: this.editVacancyForm.get('jobTitle')?.value || this.model.jobTitle,
+      noOfVacancies: this.editVacancyForm.get('noOfVacancies')?.value || this.model.noOfVacancies,
+      minimumQualification: this.editVacancyForm.get('minimumQualification')?.value || this.model.minimumQualification,
+      jobDescription: this.editVacancyForm.get('jobDescription')?.value || this.model.jobDescription,
+      experienceRequired: this.editVacancyForm.get('experienceRequired')?.value || this.model.experienceRequired,
+      minimumSalary: this.editVacancyForm.get('minimumSalary')?.value || this.model.minimumSalary,
+      maximumSalary: this.editVacancyForm.get('maximumSalary')?.value || this.model.maximumSalary,
+      publishedBy: this.model.publishedBy,
+      lastDate: this.model.lastDate,
+      publishedDate: this.model.publishedDate,
+    }
     this.email = localStorage.getItem('user-email');
     if(this.email && this.id){ 
       this.model.publishedBy = this.email;
       this.editVacancySubscription = this.employerService.updateVacancy(this.model, this.id).subscribe({
         next: (response) => {
-          this.show('Vacancy Updated Successfully!');
-          this.router.navigateByUrl('/vacancy');
+          if(response.isSuccess){
+            this.show(response.message);
+            this.router.navigateByUrl('/vacancy');
+          }
+          else{
+            this.error(response.message);
+          }
         },
         error: (error) => {
           console.error(error);
@@ -76,8 +111,13 @@ export class EditVacancyComponent implements OnInit, OnDestroy {
     if(this.id){
       this.employerService.deleteVacancy(this.id).subscribe({
         next: (response) => {
-          this.show('Vacancy Deleted Successfully!');
-          this.router.navigateByUrl('/vacancy');
+          if(response.isSuccess){
+            this.show(response.message);
+            this.router.navigateByUrl('/vacancy');
+          }
+          else{
+            this.error(response.message);
+          }
         },
         error: (error) => {
           console.error(error);
@@ -92,5 +132,9 @@ export class EditVacancyComponent implements OnInit, OnDestroy {
 
   show(msg:string) {
     this.messageService.add({ severity: 'success', summary: 'Success', detail: msg });
+  }
+  
+  error(msg:string){
+    this.messageService.add({ severity: 'error', summary: 'Error', detail: msg });
   }
 }
