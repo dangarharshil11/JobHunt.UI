c95ab0fb082507d3b3699db2bb07cd2ad8fe147f
hdangar <hdangar@synoptek.com>
2024-03-22 14:54:19 +0530

    Advanced Route Protection

--- src/app/core/interceptor/auth.interceptor.ts
+++ src/app/core/interceptor/auth.interceptor.ts
@@ -7,14 +7,27 @@ import {
 } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { CookieService } from 'ngx-cookie-service';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
+import { MessageService } from 'primeng/api';
 
 @Injectable()
 export class AuthInterceptor implements HttpInterceptor {
 
-  constructor(private cookieService: CookieService, private route: ActivatedRoute) { }
+  constructor(private cookieService: CookieService, private router: Router, private messageService: MessageService) { }
 
   intercept(request: HttpRequest<unknown>, next: HttpHandler): Observable<HttpEvent<unknown>> {
+    if(this.router.url.startsWith('/profile') || this.router.url.startsWith('/vacancy')){
+      if(!localStorage.getItem('user-roles')?.includes('Employer')){
+        this.showError('UnAuthorized');
+        this.router.navigateByUrl('');
+      }
+    }
+    else if(this.router.url.startsWith('/user') || this.router.url.startsWith('/experience') || this.router.url.startsWith('/qualification')){
+      if(!localStorage.getItem('user-roles')?.includes('JobSeeker')){
+        this.showError('UnAuthorized');
+        this.router.navigateByUrl('');
+      }    
+    }
     if (this.shouldIntercept(request)) {
       const authRequest = request.clone({
         setHeaders: {
@@ -28,5 +41,9 @@ export class AuthInterceptor implements HttpInterceptor {
 
   private shouldIntercept(request: HttpRequest<any>): boolean {
     return request.urlWithParams.indexOf('addAuth=true', 0) > -1 ? true : false;
+  }
+
+  showError(msg: string) {
+    this.messageService.add({ severity: 'error', summary: 'Error', detail: msg });
   }
 }
